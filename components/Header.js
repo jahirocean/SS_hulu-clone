e84ee42a6f68c20ef7bf4b026354aa609b38fad9
Zahir Raihan <zahirraihann@gmail.com>
2024-04-05 03:29:42 +0600

    First commit

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+
+      <Image
+        src="https://logodownload.org/wp-content/uploads/2019/09/hulu-logo.png"
+        width={100}
+        height={50}
+        className="object-contain"
+      />
+    </div>
+  );
+}
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["logodownload.org", "image.tmdb.org"],
+  },
+};
